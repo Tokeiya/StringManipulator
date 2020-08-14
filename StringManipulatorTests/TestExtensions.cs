164b5cce111ef9e71d1d4d538f21c170d3d016d1
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-08-15 01:09:43 +0900

    Implements unit tests(on going.)

---
+++ StringManipulatorTests/TestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChainingAssertion;
+
+namespace Tokeiya3.StringManipulatorTests
+{
+	static class TestExtensions
+	{
+		public static void Is(this StringBuilder actual, ReadOnlySpan<char> expected)
+		{
+			actual.Length.Is(expected.Length);
+
+			for (int i = 0; i < expected.Length; i++)
+			{
+				actual[i].Is(expected[i]);
+			}
+		}
+	}
+}
---
+++ StringManipulatorTests/ToStringBuilderTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ChainingAssertion;
+using Tokeiya3.StringManipulator;
+using Xunit;
+
+namespace Tokeiya3.StringManipulatorTests
+{
+	public class ToStringBuilderTests
+	{
+		private const string ExpectedString = "When seek the lazyness then arrive diligence";
+
+		private const string PreInputed =
+			"Machines would never tumbe fall to evil, if human being didn't make mistakes.";
+
+
+		[Fact()]
+		public void ScalarToStringBuilderTest()
+		{
+			ExpectedString.ToStringBuilder().Is(ExpectedString);
+
+			ReadOnlySpan<char> span = ExpectedString;
+			span.ToStringBuilder().Is(ExpectedString);
+
+			ReadOnlyMemory<char> mem = new ReadOnlyMemory<char>(ExpectedString.ToCharArray());
+			mem.ToStringBuilder().Is(ExpectedString);
+
+			"".ToStringBuilder().Is("");
+
+			span = new ReadOnlySpan<char>(Array.Empty<char>());
+			span.ToStringBuilder().Is("");
+
+			mem = new ReadOnlyMemory<char>(Array.Empty<char>());
+			mem.ToStringBuilder().Is("");
+
+
+		}
+
+		[Fact]
+		public void JoinToStringBuilderTest()
+		{
+			string expected = ExpectedString.Replace(' ', '\t');
+			var array = ExpectedString.Split(' ');
+
+			array.ToStringBuilder('\t').Is(expected);
+			array.ToStringBuilder("\t").Is(expected);
+
+			array.Select(x => x).ToStringBuilder('\t').Is(expected);
+			array.Select(x => x).ToStringBuilder("\t").Is(expected);
+
+			var mem = new ReadOnlyMemory<string>(array);
+			mem.ToStringBuilder('\t').Is(expected);
+			mem.ToStringBuilder("\t").Is(expected);
+
+			var span = new ReadOnlySpan<string>(array);
+			span.ToStringBuilder('\t').Is(expected);
+			span.ToStringBuilder("\t").Is(expected);
+
+			var seq = array.Select(x => x.ToCharArray().Select(y => y));
+			seq.ToStringBuilder('\t').Is(expected);
+			seq.ToStringBuilder("\t").Is(expected);
+		}
+
+		[Fact]
+		public void ScalarAppendTest()
+		{
+			const string expected = PreInputed + ExpectedString;
+
+			var bld = PreInputed.ToStringBuilder();
+			ExpectedString.Append(bld).Is(expected);
+
+			bld = PreInputed.ToStringBuilder();
+			ExpectedString.Select(c => c).Append(bld).Is(expected);
+
+			bld = PreInputed.ToStringBuilder();
+			new ReadOnlySpan<char>(ExpectedString.ToCharArray()).Append(bld).Is(expected);
+
+			bld = PreInputed.ToStringBuilder();
+			new ReadOnlyMemory<char>(ExpectedString.ToCharArray()).Append(bld).Is(expected);
+
+
+		}
+
+		[Fact]
+		public void JoinedAppendTest()
+		{
+			var expected = PreInputed + ExpectedString.Replace(' ', '\t');
+			var bld = PreInputed.ToStringBuilder();
+
+			StringBuilder reset() => bld.Clear().Append(PreInputed);
+
+
+			var array = ExpectedString.Split(' ');
+			array.Append(bld,'\t').Is(expected);
+			reset();
+			array.Append("\t").Is(expected);
+
+
+			reset();
+			array.Select(s => s).Append(bld, '\t').Is(expected);
+			reset();
+			array.Select(s=>s).Append(bld,"\t").Is(expected);
+
+			reset();
+			new ReadOnlySpan<string>(array).Append(bld,'\t').Is(expected);
+
+
+
+		}
+
+
+	}
+}
