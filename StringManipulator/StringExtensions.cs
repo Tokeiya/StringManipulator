cc29e59d2cd21394f0b65f4a509866955e4bb9af
Tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2020-08-14 18:25:41 +0900

    Designed the interface (on going)

---
+++ StringManipulator/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tokeiya3.StringManipulator
+{
+	public static class StringExtensions
+	{
+		public static StringBuilder ToStringBuilder(this string value)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+
+		}
+
+		public static StringBuilder ToStringBuilder(this IEnumerable<char> value)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+
+		}
+		public static StringBuilder ToStringBuilder(this ReadOnlySpan<char> value)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+		}
+
+		public static StringBuilder stringBuilder(this ReadOnlyMemory<char> value)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+
+		}
+
+
+		public static StringBuilder ToStringBuilder(this ReadOnlyMemory<string> value,char separator)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+
+		}
+
+		public static StringBuilder ToStringBuilder(this ReadOnlyMemory<string> value,string separator)
+		{
+#warning NotImplemented
+			throw new NotImplementedException();
+
+		}
+
+	}
+}
