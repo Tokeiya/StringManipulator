16708d26ca5bc8caa0de1b5a754859df6911f7c0
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-08-16 00:09:31 +0900

    Trivial: correct constant variable name typo.

--- StringManipulatorTests/ToStringBuilderTests.cs
+++ StringManipulatorTests/ToStringBuilderTests.cs
@@ -13,7 +13,7 @@ namespace Tokeiya3.StringManipulatorTests
 	{
 		private const string ExpectedString = "When seek the lazyness then arrive at diligence";
 
-		private const string PreInputed =
+		private const string PreInputted =
 			"Machines would never tumbe fall to evil, if human being didn't make mistakes.";
 
 
